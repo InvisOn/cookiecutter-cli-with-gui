694e8d14b1a43a7346d1fcc3aa3c1203e9121b4a
InvisOn <ajveendijk@gmail.com>
2020-09-20 13:58:09 +1200

    abstracted ui logic

--- cli_gui_template/app.py
+++ cli_gui_template/app.py
@@ -1,9 +1,7 @@
 """
 Template for a CLI with optional GUI.
 """
-from user_interface import spawn_cli, spawn_gui
+from user_interface import user_interface
 
 def main():
-    args = spawn_cli()
-    if args['graphical_user_interface']:
-        args = spawn_gui()
+    args = user_interface()
--- cli_gui_template/user_interface.py
+++ cli_gui_template/user_interface.py
@@ -1,7 +1,7 @@
 from gooey import GooeyParser, Gooey
 
 
-def cli() -> GooeyParser:
+def _cli() -> GooeyParser:
     """Creates the CLI parser without the -gui argument.
 
     Returns:
@@ -12,7 +12,7 @@ def cli() -> GooeyParser:
     return parser
 
 
-def add_gui_arg(parser: GooeyParser) -> GooeyParser:
+def _add_gui_arg(parser: GooeyParser) -> GooeyParser:
     """Adds -gui argument to a GooeyParser.
 
     Args:
@@ -22,26 +22,43 @@ def add_gui_arg(parser: GooeyParser) -> GooeyParser:
         GooeyParser:
     """
     parser.add_argument('-gui', '--graphical-user-interface',
-                        help='Spawn a GUI.',
+                        help='Spawn a graphical user interface.',
                         action='store_true')
 
     return parser
 
 
-def spawn_cli() -> GooeyParser:
+def _spawn_cli() -> GooeyParser:
     """Spawns the full command line interface (with -gui argument).
 
     Returns:
         GooeyParser:
     """
-    parser = add_gui_arg(cli())
+    parser = _add_gui_arg(_cli())
 
     return vars(parser.parse_args())
 
 
 @Gooey(program_name='Template')
-def spawn_gui():
+def _spawn_gui() -> dict:
     """Spawn a GUI based on the CLI without -gui argument.
+
+    Returns:
+        dict:
     """
     # -gui argument is not needed because the gui has already been spawned.
-    return cli().parse_args()
+    return _cli().parse_args()
+
+
+def user_interface() -> dict:
+    """Spawns the user interface based.
+
+    Returns:
+        dict: Arguments passed.
+    """
+    args = _spawn_cli()
+
+    if args['graphical_user_interface']:
+        args = _spawn_gui()
+
+    return args
